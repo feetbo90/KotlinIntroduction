c2b5ed352a27468a7ceed3c757b01ace8d0ae0e4
Muhammad Iqbal Pradipta <feetbonike90@gmail.com>
2019-02-12 19:50:51 +0700

    input list di kotlin

--- src/ListInKotlin.kt
+++ src/ListInKotlin.kt
@@ -1,4 +1,9 @@
+
+import java.util.Scanner
+
 fun main(args: Array<String>) {
+
+    var input = Scanner(System.`in`)
 
     // cara I
     var list = mutableListOf<Int>()
@@ -14,11 +19,16 @@ fun main(args: Array<String>) {
 
     list2.removeAt(2)
 
-    for (i in 0 until list2.size)
+    for (i in 0 until 5)
     {
-        println("ini list : ${list2.get(i)}")
+        list.add(input.nextInt())
+    }
+
+    for (i in 0 until list.size)
+    {
+        println("ini list : ${list.get(i)}")
     }
 
 
 
-}
+    }
