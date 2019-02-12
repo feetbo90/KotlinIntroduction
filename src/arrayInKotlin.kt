e1f4aa0259c80ac9a6d1f8d2cddc6854ee8923a8
Muhammad Iqbal Pradipta <feetbonike90@gmail.com>
2019-02-12 19:06:55 +0700

    Array di Kotlin

---
+++ src/arrayInKotlin.kt
@@ -0,0 +1,53 @@
+/*
+
+    sekumpulan nilai yang sama
+
+    array 5
+
+    0 s/d 4
+
+
+    23  44  55  89  10
+    0   1   2   3   4
+
+    array[3]
+    89
+
+ */
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+
+
+    var nilai = intArrayOf(4, 3, 20, 10)
+    var doubleNilai = doubleArrayOf(12.4, 4.5, 22.4, 32.1)
+
+    println(nilai[2])
+
+    for(i in 0 until nilai.size)
+    {
+        println("ini dari pengulangan : ${nilai[i]}")
+    }
+
+    for(i in (nilai.size-1) downTo 0){
+        println("ini dari pengulangan kedua : ${nilai[i]}")
+    }
+
+    // var namaArray = Nama_Inisialisasi(jumlah)
+    // var arrayTiga = IntArray(5)
+
+    var arrayTiga = IntArray(5)
+    var input = Scanner(System.`in`)
+
+    arrayTiga[4] = input.nextInt()
+
+    println("Hasil Input : ${arrayTiga[4]}")
+
+}
+
+/*
+    0   1   2   3   4
+                    45
+
+
+ */
