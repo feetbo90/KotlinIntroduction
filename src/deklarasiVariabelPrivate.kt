aa8ac9041c6b3851ab9f1e06f1575bff9ded9d21
Muhammad Iqbal Pradipta <feetbonike90@gmail.com>
2019-02-20 16:54:49 +0700

    private

---
+++ src/deklarasiVariabelPrivate.kt
@@ -0,0 +1,21 @@
+/*
+
+    Int
+    Double
+    Float
+    String
+    boolean
+
+    var / val nama_variabel : tipe data = value
+
+    var bilangan : Int = 12
+
+
+
+ */
+
+fun main(args: Array<String>) {
+// hallo
+
+    println("Hallo kotlin")
+}
