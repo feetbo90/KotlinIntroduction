b03f41e8a0b4dcc21665e3e2ae0368fd6d4dda82
Muhammad Iqbal Pradipta <feetbonike90@gmail.com>
2019-02-21 17:49:07 +0700

    looping private

--- src/pengulanganPrivate.kt
+++ src/pengulanganPrivate.kt
@@ -1,4 +1,4 @@
-
+import java.util.*
 
 fun main(args: Array<String>) {
 
@@ -34,4 +34,18 @@ fun main(args: Array<String>) {
     {
         println(i)
     }
+
+    var input = Scanner(System.`in`)
+    println("Masukkan bilangan : ")
+    var bilangan = input.nextInt()
+    println("Masukkan bilangan kedua : ")
+    var bilangan2 = input.nextInt()
+
+    for (i in bilangan .. bilangan2){
+        if (i % 2 == 1)
+        {
+            println(i)
+        }
+    }
+
 }
